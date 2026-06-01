c78d51b301f1e0093fe4a742ed5fab0738701fd7
Peter Kim <peter.kim427@proton.me>
2026-06-01 15:34:08 -0500

    adding table functionality

--- okstupid/maps_app.py
+++ okstupid/maps_app.py
@@ -1,11 +1,11 @@
 import json
 import os
 
-from dash import Dash, html, dcc, callback, Output, Input
-import dash_bootstrap_components as dbc
+from dash import Dash, html, dcc, callback, Output, Input, no_update, dash_table
 from fastkml import kml
 import numpy as np
 import plotly.graph_objects as go
+import polars as ps
 from pygeoif import Point, Polygon, MultiPolygon
 from shapely import (
     Point as sPoint,
@@ -143,13 +143,26 @@ process_senate_kml = _make_process_function(
     extract_lege_geometry, "./data/parsed_senate_kml.json"
 )
 
-app = Dash(__name__, requests_pathname_prefix="/isds/")
+dont_update_map = False
 
+if __name__ == "__main__":
+    app = Dash()
+else:
+    app = Dash(__name__, requests_pathname_prefix="/isds/")
+
+print("processing KML files")
 isd_geo = process_isd_kml("./data/School_Districts_2026.kml")
 house_geo = process_house_kml("./data/PlanH2316.kml")
 senate_geo = process_senate_kml("./data/PlanS2168.kml")
 
+print("done processing KML. Loading intersections file")
 intersections = create_intersections_file(isd_geo, house_geo, senate_geo)
+print("loaded intersections file")
+
+print("Loading lege membership CSVs")
+house_members_csv = ps.read_csv("./data/house_89th_lege.csv")
+senate_members_csv = ps.read_csv("./data/senate_89th_lege.csv")
+print("Loaded lege members CSVs")
 
 
 def main():
@@ -163,13 +176,23 @@ def main():
         ),
         html.Div(
             [
-                dcc.Graph(
-                    id="isd-selection-graph",
-                    figure=show_isd_map(),
-                    config={
-                        "scrollZoom": True,
-                        "modeBarButtonsToAdd": ["zoom2d"],
-                    },
+                html.Div(
+                    [
+                        dcc.Dropdown(
+                            id="isd-selection-dropdown",
+                            options=sorted(isd_geo.keys()),
+                            multi=True,
+                        ),
+                        dcc.Graph(
+                            id="isd-selection-graph",
+                            figure=show_isd_map(),
+                            config={
+                                "scrollZoom": True,
+                                "modeBarButtonsToAdd": ["zoom2d"],
+                            },
+                        ),
+                    ],
+                    style={"width": "750px", "display": "inline-block"},
                 ),
             ],
             style={"display": "inline-block"},
@@ -177,25 +200,41 @@ def main():
         html.Br(),
         html.Div(
             [
-                html.H3("overlapping house districts"),
-                dcc.Graph(id="hd-output-graph"),
-            ],
-            style={"display": "inline-block"},
-        ),
-        html.Div(
-            [
-                html.H3("overlapping senate districts"),
-                dcc.Graph(id="sd-output-graph"),
-            ],
-            style={"display": "inline-block"},
+                html.Div(
+                    [
+                        html.H3("overlapping house districts"),
+                        dcc.Graph(id="hd-output-graph"),
+                        dash_table.DataTable(
+                            id="selected-hd-table",
+                            sort_action="native",
+                        ),
+                    ],
+                    style={"display": "inline-block"},
+                ),
+                html.Div(
+                    [
+                        html.H3("overlapping senate districts"),
+                        dcc.Graph(id="sd-output-graph"),
+                        dash_table.DataTable(
+                            id="selected-sd-table",
+                            sort_action="native",
+                        ),
+                    ],
+                    style={"display": "inline-block"},
+                ),
+            ]
         ),
     ]
 
 
 @callback(
-    Output("hd-output-graph", "figure"),
-    Output("sd-output-graph", "figure"),
+    Output("hd-output-graph", "figure", allow_duplicate=True),
+    Output("sd-output-graph", "figure", allow_duplicate=True),
+    Output("selected-hd-table", "data", allow_duplicate=True),
+    Output("selected-sd-table", "data", allow_duplicate=True),
+    Output("isd-selection-dropdown", "value"),
     Input("isd-selection-graph", "selectedData"),
+    prevent_initial_call=True,
 )
 def show_isd_map_for_selection(selected_data):
     """
@@ -203,11 +242,65 @@ def show_isd_map_for_selection(selected_data):
     ISDs that were selected
     """
     if selected_data is None:
-        return go.Figure(layout={"width": 450}), go.Figure(layout={"width": 450})
+        return (
+            go.Figure(layout={"width": 450}),
+            go.Figure(layout={"width": 450}),
+            [],
+            [],
+            [],
+        )
 
+    intersecting_hds = []
     selected_isds = [elem["location"] for elem in selected_data["points"]]
+    intersecting_sds = []
+    for isd in selected_isds:
+        intersecting_hds.extend(intersections["ISD to house"][isd])
+        intersecting_sds.extend(intersections["ISD to senate"][isd])
+    intersecting_hds = list(set(intersecting_hds))
+    intersecting_sds = list(set(intersecting_sds))
+
+    hd_rows = house_members_csv.filter(
+        ps.col("District").is_in({int(n.split()[1]) for n in intersecting_hds})
+    )
+    sd_rows = house_members_csv.filter(
+        ps.col("District").is_in({int(n.split()[1]) for n in intersecting_sds})
+    )
+
+    hd_geojson = convert_polys_to_geojson(
+        {hd: house_geo[hd] for hd in intersecting_hds}
+    )
+    selected_isd_geojson = convert_polys_to_geojson(
+        {isd: isd_geo[isd] for isd in selected_isds}
+    )
+    merged_hd_geojsons = _merge_geojson_dicts(selected_isd_geojson, hd_geojson)
+    hd_fig = _make_lege_map_for_selection(
+        merged_hd_geojsons, selected_isds, intersecting_hds
+    )
+
+    sd_geojson = convert_polys_to_geojson(
+        {sd: senate_geo[sd] for sd in intersecting_sds}
+    )
+    merged_sd_geojsons = _merge_geojson_dicts(selected_isd_geojson, sd_geojson)
+    sd_fig = _make_lege_map_for_selection(
+        merged_sd_geojsons, selected_isds, intersecting_sds
+    )
+
+    global dont_update_map
+    dont_update_map = True
+    return hd_fig, sd_fig, hd_rows.to_dicts(), sd_rows.to_dicts(), selected_isds
+
+
+@callback(
+    Output("isd-selection-graph", "figure"),
+    Output("hd-output-graph", "figure"),
+    Output("sd-output-graph", "figure"),
+    Input("isd-selection-dropdown", "value"),
+)
+def update_isd_map_for_dropdown_selection(selected_isds):
     intersecting_hds = []
     intersecting_sds = []
+    if selected_isds is None:
+        selected_isds = []
     for isd in selected_isds:
         intersecting_hds.extend(intersections["ISD to house"][isd])
         intersecting_sds.extend(intersections["ISD to senate"][isd])
@@ -232,8 +325,21 @@ def show_isd_map_for_selection(selected_data):
     sd_fig = _make_lege_map_for_selection(
         merged_sd_geojsons, selected_isds, intersecting_sds
     )
+    isd_to_index = {isd: idx for (idx, isd) in enumerate(sorted(isd_geo.keys()))}
 
-    return hd_fig, sd_fig
+    global dont_update_map
+    if dont_update_map:
+        fig = no_update
+        dont_update_map = False
+    else:
+        fig = show_isd_map()
+        fig.update_traces(selectedpoints=[isd_to_index[isd] for isd in selected_isds])
+
+    return (
+        fig,
+        hd_fig,
+        sd_fig,
+    )
 
 
 def _make_lege_map_for_selection(hd_geojson, selected_isds, intersecting_hds):
@@ -253,7 +359,7 @@ def _make_lege_map_for_selection(hd_geojson, selected_isds, intersecting_hds):
 
 
 def show_isd_map():
-
+    print("creating ISD map")
     fig = go.Figure(
         layout={
             "height": 500,
@@ -272,7 +378,6 @@ def show_isd_map():
     )
     fig.add_trace(trace)
     fig.update_geos(fitbounds="locations")
-    # fig.update_layout(height=900, width=900, margin={p: 0 for p in "lrbt"})
     return fig
 
 
@@ -362,4 +467,4 @@ def plot_polygons(polygons):
 
 if __name__ == "__main__":
     main()
-    app.run(debug=False)
+    app.run(debug=True)
