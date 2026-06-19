513174267c0c2615961d565cffff343ed6a41867
Peter Kim <peter.kim427@proton.me>
2026-06-19 13:25:31 -0500

    fixing the way the blog nav buttons are generated

--- okstupid/blog/web.py
+++ okstupid/blog/web.py
@@ -18,13 +18,9 @@ def generate_blog_nav_md():
     return text
 
 
-def get_blog_url(blog_date: datetime) -> str:
-    return f"/blog/{blog_date.strftime('%m-%d-%Y')}"
-
-
-def get_prev_entry_button_html(prev_date: datetime) -> str:
+def get_prev_entry_button_html(prev_entry: data.BlogEntry) -> str:
     return f"""
-    <a href="{get_blog_url(prev_date)}">
+    <a href="/blog/entry-{prev_entry.id}">
         <img src="/static/prev-button.png"
         onmouseover="this.src='/static/prev-button-active.png'"
         onmouseout="this.src='/static/prev-button.png'"
@@ -33,9 +29,9 @@ def get_prev_entry_button_html(prev_date: datetime) -> str:
     """
 
 
-def get_next_entry_button_html(next_date: datetime) -> str:
+def get_next_entry_button_html(next_entry: data.BlogEntry) -> str:
     return f"""
-    <a href="{get_blog_url(next_date)}">
+    <a href="/blog/entry-{next_entry.id}">
         <img src="/static/next-button.png"
         onmouseover="this.src='/static/next-button-active.png'"
         onmouseout="this.src='/static/next-button.png'"
@@ -50,30 +46,30 @@ def render_link(blog_date: datetime):
 
 def generate_nav_buttons(blog_date: datetime):
     nav_html = ""
-    prev_date = get_previous_blog_entry(blog_date)
-    if prev_date is not None:
-        nav_html += get_prev_entry_button_html(prev_date)
+    prev_entry = get_previous_blog_entry(blog_date)
+    if prev_entry is not None:
+        nav_html += get_prev_entry_button_html(prev_entry)
 
-    next_date = get_next_blog_entry(blog_date)
-    if next_date is not None:
-        nav_html += get_next_entry_button_html(next_date)
+    next_entry = get_next_blog_entry(blog_date)
+    if next_entry is not None:
+        nav_html += get_next_entry_button_html(next_entry)
 
     return nav_html
 
 
-def get_previous_blog_entry(blog_date: datetime) -> datetime | None:
+def get_previous_blog_entry(blog_date: datetime) -> data.BlogEntry | None:
     db = data.get_sql_connection()
     all_entries = data.load_blog_entries(db)
     for entry in sorted(all_entries, key=lambda e: e.create_date)[::-1]:
         if entry.create_date < blog_date:
-            return entry.create_date
+            return entry
     return None
 
 
-def get_next_blog_entry(blog_date: datetime) -> datetime | None:
+def get_next_blog_entry(blog_date: datetime) -> data.BlogEntry | None:
     db = data.get_sql_connection()
     all_entries = data.load_blog_entries(db)
     for entry in sorted(all_entries, key=lambda e: e.create_date):
         if entry.create_date > blog_date:
-            return entry.create_date
+            return entry
     return None
